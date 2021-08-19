67441b5cbd9c0fcb1aff769f6e6040a0ec727eb8
Maria Sol Garay <mariasolgaray01@gmail.com>
2021-08-19 20:45:55 -0300

    login

--- PregunChaco_App/PregunChaco_App/urls.py
+++ PregunChaco_App/PregunChaco_App/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homeVista, name='INFO'),
-
+    path('', views.homeVista, name='home'),
+    path('login/',auth.LoginView.as_view(template_name = 'usuarios/login.html'), name= 'login'),
+    path('logout/', auth.LogoutView.as_view(), name = 'logout'),
 ]
