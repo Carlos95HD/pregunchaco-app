4ecba53fac4a8563424043ac15c9199835501977
Maria Sol Garay <mariasolgaray01@gmail.com>
2021-08-25 15:11:52 -0300

    'preguntas'

--- PregunChaco_App/apps/pregunta/urls.py
+++ PregunChaco_App/apps/pregunta/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import listar_categoria
+from .views import listar_categoria, listar_pregunta
 
 app_name = 'pregunta'
 
 urlpatterns = [
 	path('categoria/', listar_categoria, name = 'categoria'),
+	path('pregunta/', listar_pregunta, name = 'preguntas')
 ]
--- PregunChaco_App/apps/pregunta/views.py
+++ PregunChaco_App/apps/pregunta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import categoria
+from .models import categoria, Pregunta
 from django.views.generic import DetailView
 # Create your views here.
 
@@ -7,3 +7,11 @@ def listar_categoria(request):
     categorias = categoria.objects.all()
     context = {"categorias":categorias}
     return render(request,'game/categorias.html', context)
+
+
+
+
+def listar_pregunta(request):
+ 	preguntas = Pregunta.objects.all()
+ 	context = {"preguntas":Pregunta}
+ 	return render(request, 'game/preguntas.html', context)
