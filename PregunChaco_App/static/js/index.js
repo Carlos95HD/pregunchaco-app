6294bfae189847ab38d1237f189bd2b80aa35776
Carlos <hernandeasi7@gmail.com>
2021-08-29 22:44:43 -0300

    Añadido | index.js

---
+++ PregunChaco_App/static/js/index.js
@@ -0,0 +1,23 @@
+// const submitRespuesta = document.querySelector('#btn-submit')
+// const opcionRespuesta = document.getElementsByName('respuesta_pk');
+// const formRespuesta = document.querySelector('#form-respuesta')
+
+// formRespuesta.addEventListener('submit', (e) => {
+//   console.log( e.target.value )
+//   check()
+//   function check() {
+//     var r = document.getElementsByName("respuesta_pk")
+//     var c = -1
+    
+//     for(var i=0; i < r.length; i++){
+//        if(r[i].checked) {
+//           c = i; 
+//        }
+//     }
+//     if (c == -1) {
+//       e.preventDefault()
+//       alert("Selecciones una opción");
+//     } 
+//   }
+
+// })
